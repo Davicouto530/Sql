de5c692d62b63df63eb1d9a47b770233ea5ecd91
Davi do Couto <coutodavi530@gmail.com>
2026-03-19 16:10:42 -0300

    Deixando o código enxuto.

--- AulasUdemy/EstadoCidadeMunicipio/SQLQuery1.sql
+++ AulasUdemy/EstadoCidadeMunicipio/SQLQuery1.sql
@@ -7,13 +7,12 @@ select
 m.id		as id_municipio,
 m.codigo	as cod_municipio,
 m.nome		as nome_municipio,
-m.uf		as uf_municipio,
-e.CodigoUf	as cod_UF,
 e.Uf		as UF,
 e.Nome		as nome_estado,
-e.Regiao	as cod_regiao,
-r.id		as id_regiao,
 r.nome		as nome_estado
-from ##municipio as m 
+into ##testeMun from ##municipio as m 
 left join ##estado as e on m.uf = e.uf
 left join ##regiao as r on e.regiao = r.id 
+
+select * from ##testeMun
+where uf_estado = 'Sp'
