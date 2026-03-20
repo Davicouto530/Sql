8124652de0f3626665ad0a377a30c236e8237ef9
Davi do Couto <coutodavi530@gmail.com>
2026-03-20 15:42:15 -0300

    Criando a tabela.

--- AulasUdemy/EstadoCidadeMunicipio/SQLQuery1.sql
+++ AulasUdemy/EstadoCidadeMunicipio/SQLQuery1.sql
@@ -1,18 +1,21 @@
-select * from ##regiao
-select * from ##estado
-select * from ##municipio
+select * from regiao
+select * from estado
+select * from municipio
 
--- consulta estruturada em todas as tabelas
+-- consulta estruturada resumida em todas as tabelas
 select 
 m.id		as id_municipio,
 m.codigo	as cod_municipio,
 m.nome		as nome_municipio,
-e.Uf		as UF,
+e.Uf		as UF_estado,
 e.Nome		as nome_estado,
-r.nome		as nome_estado
-into ##testeMun from ##municipio as m 
-left join ##estado as e on m.uf = e.uf
-left join ##regiao as r on e.regiao = r.id 
+r.nome		as nome_regiao
+into Dados_Sp from municipio as m 
+left join estado as e on m.uf = e.uf
+left join regiao as r on e.regiao = r.id 
+where e.UF = 'Sp'
 
-select * from ##testeMun
-where uf_estado = 'Sp'
+select * from Dados_Sp
+
+-- drop table Dados_Sp
+-- drop table Dados_Completos
